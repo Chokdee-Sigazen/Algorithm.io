f71085e320906673a9348f7055eb74bc0856387a
Sigazen <choikdee50@gmail.com>
2023-11-23 02:17:07 +0700

    adding dist

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import "./App.css";
 import { HashRouter } from "react-router-dom";
 import NavBar from "./component/TopBar";
 import Footer from "./component/Footer";
-import AnimatorRoute from "./AnimatorRoute";
+import { Routes, Route, useLocation } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Quiz from "./pages/Quiz";
 
 function App() {
+  const location = useLocation();
   return (
     <HashRouter>
       <NavBar />
       <main>
-        <AnimatorRoute />
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
       </main>
       <footer>
         <Footer />
