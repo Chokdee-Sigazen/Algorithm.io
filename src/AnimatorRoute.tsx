6e49070312936cb58e2f8b429e1106a85bec057a
Sigazen <choikdee50@gmail.com>
2023-11-23 02:20:24 +0700

    adding dist

---
+++ src/AnimatorRoute.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Quiz from "./pages/Quiz";
+import { AnimatePresence } from "framer-motion";
+const AnimatorRoute = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+export default AnimatorRoute;
